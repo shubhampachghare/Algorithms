551b220a059d2fccca9335084fecd10869efcf82
shubhampachghare <shubhampachghare@gmail.com>
2023-03-31 00:26:41 +0530

    [Shubham] -Bubble Sort

---
+++ AlgorithmPrograms/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithamPrograms
+{
+    class BubbleSort
+    {
+        public static void doBubbleSort(int[] array)
+        {
+            int temp;
+            Console.WriteLine("The original Array is:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                for (int i = 0; i < array.Length -1; i++)
+                {
+                    //elements in array are: 5, 2, 6, 
+                    if (array[i] > array[i + 1])
+                    {
+                        temp = array[i + 1]; //temp=2
+                        array[i + 1] = array[i]; //  5 2 6
+                        array[i] = temp; // 2 5 6
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AlgorithamPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] array = { 5, 0, 3, 9, 6, 2, 1 };
+                BubbleSort.doBubbleSort(array);
+                Console.WriteLine("Sorted Array");
+                foreach (int i in array)
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
